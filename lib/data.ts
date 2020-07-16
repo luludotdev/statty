1e8c540d74dba512301c555e10c21723c8341474
Jack Baron <jackmbaron@gmail.com>
2020-07-16 14:56:50 +0100

    single stats API route

--- lib/data.ts
+++ lib/data.ts
@@ -5,3 +5,13 @@ export const fetchData: () => Promise<ITransformedData[]> = async () => {
   const managers = await getManagers()
   return [...managers.values()].map(x => transformData(x))
 }
+
+export const fetchDataSingle: (
+  manager: string
+) => Promise<ITransformedData | undefined> = async name => {
+  const managers = await getManagers()
+  const manager = managers.get(name)
+
+  if (manager === undefined) return undefined
+  return transformData(manager)
+}
--- pages/api/stats/[stat].ts
+++ pages/api/stats/[stat].ts
@@ -1,6 +1,19 @@
+import { fetchDataSingle } from 'lib/data'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(502)
-  res.end()
+const stats = async (request: NextApiRequest, resp: NextApiResponse) => {
+  const stat = Array.isArray(request.query.stat)
+    ? request.query.stat[0]
+    : request.query.stat
+
+  const data = await fetchDataSingle(stat)
+  if (data === undefined) {
+    resp.statusCode = 404
+    return resp.end()
+  }
+
+  resp.statusCode = 200
+  resp.json(data)
 }
+
+export default stats
