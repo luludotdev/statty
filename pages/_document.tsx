ad5b4a81cc28cf4c8c16a5382b5ad5b686029b68
Jack Baron <jackmbaron@gmail.com>
2020-07-16 23:53:45 +0100

    add html lang

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
+
+export default class NextDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <Head />
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
