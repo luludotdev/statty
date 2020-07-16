a5a26be2ae00eefa9554f2de9eed0079c98e7634
Jack Baron <jackmbaron@gmail.com>
2020-07-16 15:38:52 +0100

    register environment variables

---
+++ lib/env/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { registerInt, registerString } from "./register"
+
+const NODE_ENV = registerString('NODE_ENV')
+const IS_PROD = NODE_ENV?.toLowerCase() === 'production'
+export const IS_DEV = !IS_PROD
+
+// #region Redis
+export const REDIS_HOST = registerString('REDIS_HOST') ?? (IS_DEV ? 'localhost' : 'redis')
+export const REDIS_PORT = registerInt('REDIS_PORT') ?? 6379
+export const REDIS_PASS = registerString('REDIS_PASS')
+export const REDIS_DB_BASE = registerInt('REDIS_DB_BASE') ?? 0
+// #endregion
