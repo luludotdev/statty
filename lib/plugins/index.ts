c9349c8c8b8256477ef0720e2edeb47f2477857b
Jack Baron <jackmbaron@gmail.com>
2020-07-16 15:09:55 +0100

    export status from plugins

--- lib/plugins/index.ts
+++ lib/plugins/index.ts
@@ -1,5 +1,5 @@
 export type { IPlugin, IPluginReponse, IPluginFactory } from './base'
 
-export { IS_FACTORY } from './base'
+export { IS_FACTORY, Status } from './base'
 export { httpFactory } from './http'
 export { pingFactory } from './ping'
