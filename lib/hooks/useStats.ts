137b64b0cff6b4f40cf4472ef8a674376a3783de
Jack Baron <jackmbaron@gmail.com>
2020-07-16 17:08:56 +0100

    create stats hook

---
+++ lib/hooks/useStats.ts
@@ -0,0 +1,21 @@
+import useSWR, { responseInterface } from 'swr'
+import { Except } from 'type-fest'
+import { ITransformedData } from '~managers'
+import { axios } from '~utils/axios'
+
+export const fetchStats: () => Promise<ITransformedData> = async () => {
+  const resp = await axios.get<ITransformedData>('/api/stats')
+  return resp.data
+}
+
+type Response = Except<responseInterface<any, any>, 'data'>
+
+/* eslint-disable prettier/prettier */
+export function useStats(): Response & { stats: ITransformedData | undefined }
+export function useStats(initialData: ITransformedData): Response & { stats: ITransformedData }
+export function useStats(initialData?: ITransformedData): Response & { stats: ITransformedData | undefined } {
+  const resp = useSWR('/stats', fetchStats, { initialData })
+  return {...resp, stats: resp.data }
+}
+
+/* eslint-enable prettier/prettier */
