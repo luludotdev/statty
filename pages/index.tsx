3cef37af6f08fda38b149c81a446bfd667c25242
Jack Baron <jackmbaron@gmail.com>
2020-07-16 17:09:09 +0100

    populate app with stats and site name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
+import type { IInstance } from '~api/instance'
+import { Meta } from '~components/Meta'
+import { fetchStats, useStats } from '~hooks/useStats'
+import { ITransformedData } from '~managers'
+import { axios } from '~utils/axios'
 
-const App: NextPage = () => {
-  return null
+interface IProps {
+  siteName: string
+  initialData: ITransformedData
+}
+
+const App: NextPage<IProps> = ({ siteName, initialData }) => {
+  const { stats } = useStats(initialData)
+
+  return (
+    <>
+      <Meta siteName={siteName} />
+      <pre>{JSON.stringify(stats, null, 2)}</pre>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<IProps> = async () => {
+  const [resp, initialData] = await Promise.all([
+    axios.get<IInstance>('/api/instance'),
+    fetchStats(),
+  ])
+
+  return { props: { siteName: resp.data.name, initialData } }
 }
 
 export default App
