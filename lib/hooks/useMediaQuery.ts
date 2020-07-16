8077670d6a8e680b8f16918d517084e2bcc8e674
Jack Baron <jackmbaron@gmail.com>
2020-07-16 23:04:46 +0100

    don't run media query hooks on server

--- lib/hooks/useMediaQuery.ts
+++ lib/hooks/useMediaQuery.ts
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react'
+import { IS_SERVER } from '~env'
 
 export const useMediaQuery = (query: string) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  return IS_SERVER ? false : useMediaQueryClient(query)
+}
+
+const useMediaQueryClient = (query: string) => {
   const mql = window.matchMedia(query)
   const [matches, setMatches] = useState(mql.matches)
 
