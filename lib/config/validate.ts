64398976ce07fa839cdcdd23f8e5ef70065933b8
Jack Baron <jackmbaron@gmail.com>
2020-07-19 14:19:20 +0100

    include validation failures in exit error

--- lib/config/validate.ts
+++ lib/config/validate.ts
@@ -14,13 +14,22 @@ const isValid: (object: any) => object is IConfig = object => {
   return valid
 }
 
+class ValidationError extends Error {
+  public readonly errors: Ajv.ErrorObject[] | null | undefined
+
+  constructor(errors: Ajv.ErrorObject[] | null | undefined) {
+    super('Invalid config!')
+    this.errors = errors
+  }
+}
+
 export const validateConfig: (object: any) => IConfig = object => {
   const error = new Error('Invalid config!')
   if (typeof object !== 'object') throw error
   if (object === null) throw error
 
   const valid = isValid(object)
-  if (valid === false) throw error
+  if (valid === false) throw new ValidationError(validate.errors)
 
   delete object.$schema
   return object
