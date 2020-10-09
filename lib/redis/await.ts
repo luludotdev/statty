bf09997f205fdf8a92c42f004e55f1fb8640a83c
Jack Baron <jackmbaron@gmail.com>
2020-10-09 02:40:04 +0100

    fix lint failure

--- lib/redis/await.ts
+++ lib/redis/await.ts
@@ -2,6 +2,6 @@ import { redis } from './db'
 
 export const awaitRedis = async () =>
   new Promise(resolve => {
-    if (redis.status === 'ready') return resolve()
-    redis.on('ready', () => resolve())
+    if (redis.status === 'ready') resolve()
+    else redis.on('ready', () => resolve())
   })
