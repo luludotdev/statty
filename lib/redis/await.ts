6af7f02925ad67d7c44dfd56677e6aa5494ba172
Jack Baron <jackmbaron@gmail.com>
2020-12-13 11:12:53 +0000

    fix typescript breaking

--- lib/redis/await.ts
+++ lib/redis/await.ts
@@ -1,6 +1,6 @@
 import { redis } from './db'
 
-export const awaitRedis = async () =>
+export const awaitRedis: () => Promise<void> = async () =>
   new Promise(resolve => {
     if (redis.status === 'ready') resolve()
     else redis.on('ready', () => resolve())
