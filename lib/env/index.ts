2ac489f81940a4559d4f1901f9c7bd5c82e67f83
Jack Baron <jackmbaron@gmail.com>
2021-05-24 12:31:03 +0100

    support REDIS_URL env var

--- lib/env/index.ts
+++ lib/env/index.ts
@@ -14,6 +14,7 @@ export const CONFIG_PATH = registerString('CONFIG_PATH')
 // #endregion
 
 // #region Redis
+export const REDIS_URL = registerString('REDIS_URL')
 export const REDIS_HOST = registerString('REDIS_HOST') ?? (IS_DEV ? 'localhost' : 'redis')
 export const REDIS_PORT = registerInt('REDIS_PORT') ?? 6379
 export const REDIS_PASS = registerString('REDIS_PASS')
--- lib/redis/db.ts
+++ lib/redis/db.ts
@@ -1,14 +1,28 @@
 import Redis from 'ioredis'
 import { schedule } from 'node-cron'
-import { REDIS_DB_BASE, REDIS_HOST, REDIS_PASS, REDIS_PORT } from '~env'
+import {
+  REDIS_DB_BASE,
+  REDIS_HOST,
+  REDIS_PASS,
+  REDIS_PORT,
+  REDIS_URL,
+} from '~env'
 
-export const redis = new Redis({
-  db: REDIS_DB_BASE + 0,
-  host: REDIS_HOST,
-  password: REDIS_PASS,
-  port: REDIS_PORT,
-})
+const createRedis = () => {
+  if (REDIS_URL !== undefined) {
+    const redis = new Redis(REDIS_URL)
+    return redis
+  }
 
+  return new Redis({
+    db: REDIS_DB_BASE + 0,
+    host: REDIS_HOST,
+    password: REDIS_PASS,
+    port: REDIS_PORT,
+  })
+}
+
+export const redis = createRedis()
 redis.on('error', () => {
   console.log('Failed to connect to Redis')
   process.exit(1)
